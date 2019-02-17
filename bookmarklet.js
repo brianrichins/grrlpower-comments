784314af088bdb7e6722771769ee52606e34296c
Brian <brianrichins@users.noreply.github.com>
2019-02-16 17:46:20 -0700

    Updated bookmarklet to renamed file location
    jsDelivr is pretty aggressive about caching, appear to cache for 7+ days with no way to clear

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){var ref=document.createElement('script');ref.type="text/javascript";ref.src="https://cdn.jsdelivr.net/gh/brianrichins/grrlpower-comments@master/comments.min.js";document.getElementsByTagName("head")[0].appendChild(ref)})()
+javascript:(function(){var ref=document.createElement('script');ref.type="text/javascript";ref.src="https://cdn.jsdelivr.net/gh/brianrichins/grrlpower-comments@master/script.min.js";document.getElementsByTagName("head")[0].appendChild(ref)})()
